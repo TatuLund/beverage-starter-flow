1e6d775d8dc5dcffcaf792306e5b81e5bd91dd2d
Manolo Carrasco <manolo@apache.org>
2019-07-19 13:01:52 +0200

    wip

---
+++ frontend/index.js
@@ -0,0 +1,40 @@
+
+
+import '@vaadin/vaadin-lumo-styles/typography.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+
+// @ts-ignore
+import { Router } from '@vaadin/router/index.js';
+import './src/main-layout';
+
+const routes = [
+    {
+        path: '/',
+        component: 'main-layout',
+        children: [
+            {
+                path: '/client-categories',
+                title: 'Client Categories',
+                component: 'client-categories',
+                action: async () => {
+                    await import('./client-categories');
+                }
+            },
+            {
+                // fallback to Flow if no client-side routes match
+                path: '(.*)',
+                action: async (context) => {
+                    await import('../target/frontend/generated-flow-imports');
+
+                    // const { flow } = await import('@vaadin/flow');
+                    // const app = await flow.embed();
+                    // return app.render(context.pathname);
+                }
+            }
+        ]
+    }
+];
+
+const router = new Router(document.querySelector('#outlet'));
+router.setRoutes(routes);
